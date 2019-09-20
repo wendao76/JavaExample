3a71ecdbf80e32c65bef05423cdcddbf3dcef548
李文虎 <liwenhu@cyou-inc.com>
2019-09-20 18:00:45 +0800

    aio样例添加

--- src/main/java/com/github/wendao76/io/bio/IMClient.java
+++ src/main/java/com/github/wendao76/io/bio/IMClient.java
@@ -1,4 +1,27 @@
 package com.github.wendao76.io.bio;
 
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Date;
+import java.util.Scanner;
+
 public class IMClient {
+    public static void main(String[] args) {
+        // TODO 创建多个线程，模拟多个客户端连接服务端
+        new Thread(() -> {
+            try {
+                Socket socket = new Socket("127.0.0.1", 3333);
+                Scanner scanner = new Scanner(System.in);
+                while (true) {
+                    try {
+                        String meg = scanner.nextLine();
+                        socket.getOutputStream().write(meg.getBytes());
+                        Thread.sleep(2000);
+                    } catch (Exception e) {
+                    }
+                }
+            } catch (IOException e) {
+            }
+        }).start();
+    }
 }
---
+++ src/main/java/com/github/wendao76/io/bio/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * BIO样例开发
+ */
+package com.github.wendao76.io.bio;
