0a8540f1d02febf101f0f33cb9862c9e19606acc
李文虎 <liwenhu@cyou-inc.com>
2019-12-10 18:51:47 +0800

    栈帧代码查看

--- src/main/java/com/github/wendao76/jvm/TestStackFrame.java
+++ src/main/java/com/github/wendao76/jvm/TestStackFrame.java
@@ -1,12 +1,25 @@
 package com.github.wendao76.jvm;
 
+/**
+ * @author wendao76
+ */
 public class TestStackFrame {
+    final int PORT = 3306;
     public static void main(String[] args) {
         TestStackFrame testStackFrame = new TestStackFrame();
         System.out.println(testStackFrame.add(1, 2));
+        System.out.println(testStackFrame.add(1.5, 2.7));
     }
 
     public int add(int a, int b){
         return a + b;
+    }
+
+    public double add(double a, double b) {
+        return a + b + PORT;
+    }
+
+    public void init() {
+
     }
 }
